09fd3e581c7d5be5eec7540766168db3c7b47889
Ricardo <ricardo.grandon@gmail.com>
2019-04-09 09:17:06 -0400

    Update Oracle.php
    remove boolean in tables

--- src/OAuth2/Storage/Oracle.php
+++ src/OAuth2/Storage/Oracle.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace OAuth2\Storage;
+
 use OAuth2\OpenID\Storage\UserClaimsInterface;
 use OAuth2\OpenID\Storage\AuthorizationCodeInterface as OpenIDAuthorizationCodeInterface;
+
 /**
  * Simple PDO storage for all storage types
  *
@@ -28,6 +31,7 @@ class Oracle implements
 {
     protected $db;
     protected $config;
+
     public function __construct($connection, $config = array()){
         if (!is_resource($connection)) {
             if (is_string($connection)) {
@@ -48,6 +52,7 @@ class Oracle implements
             $connection = oci_connect($connection['username'], $connection['password'],$connection['dsn'], 'WE8ISO8859P1');
         }
         $this->db = $connection;
+
 	     $this->config = array_merge(array(
             'client_table' => 'oauth_clients',
             'access_token_table' => 'oauth_access_tokens',
@@ -60,6 +65,7 @@ class Oracle implements
             'public_key_table'  => 'oauth_public_keys',
         ), $config);
     }
+
     /* OAuth2\Storage\ClientCredentialsInterface */
     public function checkClientCredentials($client_id, $client_secret = null){
         $stmt = oci_parse($this->db, sprintf('SELECT * from %s where client_id = :client_id', $this->config['client_table']));
@@ -68,6 +74,8 @@ class Oracle implements
         $result = @array_change_key_case(oci_fetch_assoc($stmt),CASE_LOWER);
         return $result && $result['client_secret'] == $client_secret;
     }
+
+
     public function isPublicClient($client_id){
         $stmt = oci_parse($this->db, sprintf('SELECT * from %s where client_id = :client_id', $this->config['client_table']));
         oci_bind_by_name($stmt,':client_id',$client_id);
@@ -78,6 +86,8 @@ class Oracle implements
         }
         return empty($result['client_secret']);
     }
+
+
     /* OAuth2\Storage\ClientInterface */
     public function getClientDetails($client_id){
         $stmt = oci_parse($this->db, sprintf('SELECT * from %s where client_id = :client_id', $this->config['client_table']));
@@ -86,6 +96,8 @@ class Oracle implements
         $result = @array_change_key_case(oci_fetch_assoc($stmt),CASE_LOWER);
         return $result;
     }
+
+
     public function setClientDetails($client_id, $client_secret = null, $redirect_uri = null, $grant_types = null, $scope = null, $user_id = null){
         // if it exists, update it.
         if ($this->getClientDetails($client_id)) {
@@ -93,6 +105,7 @@ class Oracle implements
         } else {
             $stmt = oci_parse($this->db, sprintf('INSERT INTO %s (client_id, client_secret, redirect_uri, grant_types, scope, user_id) VALUES (:client_id, :client_secret, :redirect_uri, :grant_types, :scope, :user_id)', $this->config['client_table']));
         }
+
         oci_bind_by_name($stmt,':client_id',$client_id);
         oci_bind_by_name($stmt,':client_secret',$client_secret);
         oci_bind_by_name($stmt,':redirect_uri',$redirect_uri);
@@ -105,15 +118,21 @@ class Oracle implements
         }
         return true;
     }
+
+
     public function checkRestrictedGrantType($client_id, $grant_type){
         $details = $this->getClientDetails($client_id);
         if (isset($details['grant_types'])) {
             $grant_types = explode(' ', $details['grant_types']);
+
             return in_array($grant_type, (array) $grant_types);
         }
         // if grant_types are not defined, then none are restricted
         return true;
     }
+
+
+
     /* OAuth2\Storage\AccessTokenInterface */
     public function getAccessToken($access_token){
         $stmt = oci_parse($this->db, sprintf('SELECT * from %s where access_token = :access_token', $this->config['access_token_table']));
@@ -125,44 +144,57 @@ class Oracle implements
         }
         return $token;
     }
+
+
+
     public function setAccessToken($access_token, $client_id, $user_id, $expires, $scope = null){
         // convert expires to datestring
+
         $expires = date('Y-m-d H:i:s', $expires);
         // if it exists, update it.
         if ($this->getAccessToken($access_token)) {
         	$strSql = sprintf('UPDATE %s SET client_id=:client_id, expires=:expires, user_id=:user_id, scope=:scope where access_token=:access_token', $this->config['access_token_table']);
             $stmt = oci_parse($this->db, $strSql);
         } else {
-        	$strSql = sprintf('INSERT INTO %s (access_token, client_id, expires, user_id, scope) VALUES (:access_token, :client_id, :expires, :user_id, :scope)',$this->config['access_token_table']);
-            $stmt = oci_parse($this->db,$strSql);
+			
+        	$strSql = sprintf("INSERT INTO %s (access_token, client_id, expires, user_id, scope) VALUES (:access_token, :client_id, TO_TIMESTAMP(:expires, 'YYYY-MM-DD HH24:MI:SS.FF'), :user_id, :scope)",$this->config['access_token_table']);
+			$stmt = oci_parse($this->db,$strSql);
         }
+
+
         oci_bind_by_name($stmt,':access_token',$access_token);
         oci_bind_by_name($stmt,':client_id',$client_id);
         oci_bind_by_name($stmt,':user_id',$user_id);
-        oci_bind_by_name($stmt,':expires',$expires);
+		oci_bind_by_name($stmt,':expires',$expires);
         oci_bind_by_name($stmt,':scope',$scope);
         if(oci_execute($stmt)){
         	 return true;
         }
         print_r(oci_error($stmt));
         return false;
+
+
     }
+
     public function unsetAccessToken($access_token){
         $stmt = oci_parse($this->db, sprintf('DELETE FROM %s WHERE access_token = :access_token', $this->config['access_token_table']));
         oci_bind_by_name($stmt,':access_token',$access_token);
         return  oci_execute($stmt);
     }
+
     /* OAuth2\Storage\AuthorizationCodeInterface */
     public function getAuthorizationCode($code){
         $stmt = oci_parse($this->db, sprintf('SELECT * from %s where authorization_code = :code', $this->config['code_table']));
         oci_bind_by_name($stmt,':code',$code);
         oci_execute($stmt);
+
         if ($code = @array_change_key_case(oci_fetch_assoc($stmt),CASE_LOWER)) {
             // convert date string back to timestamp
             $code['expires'] = strtotime($code['expires']);
         }
         return $code;
     }
+
     public function setAuthorizationCode($code, $client_id, $user_id, $redirect_uri, $expires, $scope = null, $id_token = null){
         if (func_num_args() > 6) {
             // we are calling with an id token
@@ -170,12 +202,14 @@ class Oracle implements
         }
         // convert expires to datestring
         $expires = date('Y-m-d H:i:s', $expires);
+
         // if it exists, update it.
         if ($this->getAuthorizationCode($code)) {
             $stmt = oci_parse($this->db, $sql = sprintf('UPDATE %s SET client_id=:client_id, user_id=:user_id, redirect_uri=:redirect_uri, expires=:expires, scope=:scope where authorization_code=:code', $this->config['code_table']));
         } else {
             $stmt = oci_parse($this->db, sprintf('INSERT INTO %s (authorization_code, client_id, user_id, redirect_uri, expires, scope) VALUES (:code, :client_id, :user_id, :redirect_uri, :expires, :scope)', $this->config['code_table']));
         }
+
         oci_bind_by_name($stmt,':code',$code);
         oci_bind_by_name($stmt,':client_id',$client_id);
         oci_bind_by_name($stmt,':user_id',$user_id);
@@ -188,9 +222,11 @@ class Oracle implements
 		}
         return true;
     }
+
     private function setAuthorizationCodeWithIdToken($code, $client_id, $user_id, $redirect_uri, $expires, $scope = null, $id_token = null){
         // convert expires to datestring
         $expires = date('Y-m-d H:i:s', $expires);
+
         // if it exists, update it.
         if ($this->getAuthorizationCode($code)) {
             $stmt = oci_parse($this->db, $sql = sprintf('UPDATE %s SET client_id=:client_id, user_id=:user_id, redirect_uri=:redirect_uri, expires=:expires, scope=:scope, id_token =:id_token where authorization_code=:code', $this->config['code_table']));
@@ -210,6 +246,7 @@ class Oracle implements
         }
         return true;
     }
+
     public function expireAuthorizationCode($code){
         $stmt = oci_parse($this->db, sprintf('DELETE FROM %s WHERE authorization_code = :code', $this->config['code_table']));
         oci_bind_by_name($stmt,':code',$code);
@@ -219,6 +256,7 @@ class Oracle implements
         }
         return true;
     }
+
     /* OAuth2\Storage\UserCredentialsInterface */
     public function checkUserCredentials($username, $password){
         if ($user = $this->getUser($username)) {
@@ -226,16 +264,20 @@ class Oracle implements
         }
         return false;
     }
+
     public function getUserDetails($username){
         return $this->getUser($username);
     }
+
     /* UserClaimsInterface */
     public function getUserClaims($user_id, $claims){
         if (!$userDetails = $this->getUserDetails($user_id)) {
             return false;
         }
+
         $claims = explode(' ', trim($claims));
         $userClaims = array();
+
         // for each requested claim, if the user has the claim, set it in the response
         $validClaims = explode(' ', self::VALID_CLAIMS);
         foreach ($validClaims as $validClaim) {
@@ -248,17 +290,23 @@ class Oracle implements
                 }
             }
         }
+
         return $userClaims;
     }
+
     protected function getUserClaim($claim, $userDetails){
         $userClaims = array();
         $claimValuesString = constant(sprintf('self::%s_CLAIM_VALUES', strtoupper($claim)));
         $claimValues = explode(' ', $claimValuesString);
+
         foreach ($claimValues as $value) {
             $userClaims[$value] = isset($userDetails[$value]) ? $userDetails[$value] : null;
         }
+
         return $userClaims;
     }
+
+
     /* OAuth2\Storage\RefreshTokenInterface */
     public function getRefreshToken($refresh_token){
         $stmt = oci_parse($this->db, sprintf('SELECT * FROM %s WHERE refresh_token = :refresh_token', $this->config['refresh_token_table']));
@@ -270,6 +318,9 @@ class Oracle implements
         }
         return $token;
     }
+
+
+
     public function setRefreshToken($refresh_token, $client_id, $user_id, $expires, $scope = null){
         // convert expires to datestring
         $expires = date('Y-m-d H:i:s', $expires);
@@ -285,6 +336,7 @@ class Oracle implements
         }
         return true;
     }
+
     public function unsetRefreshToken($refresh_token){
         $stmt = oci_parse($this->db, sprintf('DELETE FROM %s WHERE refresh_token = :refresh_token', $this->config['refresh_token_table']));
         oci_bind_by_name($stmt,':refresh_token',$refresh_token);
@@ -293,7 +345,11 @@ class Oracle implements
         	return false;
         }
         return true;
+
     }
+
+
+
     /**
      * This function was modified to work with the same SGL patterns
      *   Student authentication using library password( from pergamum)
@@ -314,9 +370,11 @@ class Oracle implements
     	if(!oci_fetch_assoc($stmt)){
 			return false;
     	}
+
     	return true;
  	        //return $user['password'] == sha1($password);
     }
+
     public function getUser($username) {
     	$sql = sprintf('SELECT * from %s where username=:username', $this->config['user_table']);
         $stmt = oci_parse($this->db, $sql);
@@ -327,11 +385,14 @@ class Oracle implements
         if (!$userInfo) {
             return false;
         }
+
         // the default behavior is to use "username" as the user_id
         return array_merge(array(
             'user_id' => $username
         ), $userInfo);
     }
+
+
     /* ScopeInterface */
     public function scopeExists($scope){
         $scopes = explode(' ', $scope);
@@ -339,6 +400,7 @@ class Oracle implements
 		foreach($scopes as $scope){
 			$params[] = ':'.$scope;
 		}
+
         $stmt = oci_parse($this->db, sprintf('SELECT count(scope) as count FROM %s WHERE scope IN (%s)', $this->config['scope_table'], implode(', ',$params)));
         foreach($params as $param){
         	oci_bind_by_name($stmt,$param,str_replace(':','',$param));
@@ -347,45 +409,58 @@ class Oracle implements
         if ($result = array_change_key_case(oci_fetch_assoc($stmt),CASE_LOWER)) {
             return $result['count'] == count($scopes);
         }
+
         return false;
     }
+
     public function getDefaultScope($client_id = null) {
         $stmt = oci_parse($this->db, sprintf('SELECT scope FROM %s WHERE is_default=\'Y\'', $this->config['scope_table']));
         oci_execute($stmt);
         if ($result = @array_change_key_case(oci_fetch_assoc($stmt),CASE_LOWER)) {
+
             $defaultScope = array_map(function ($row) {
                 return $row['scope'];
             }, $result);
+
             return implode(' ', $defaultScope);
         }
+
         return null;
     }
+
     /* JWTBearerInterface */
     public function getClientKey($client_id, $subject){
         $stmt = oci_parse($this->db, $sql = sprintf('SELECT public_key from %s where client_id=:client_id AND subject=:subject', $this->config['jwt_table']));
         oci_bind_by_name($stmt,':client_id',$client_id);
         oci_bind_by_name($stmt,':subject',$subject);
         oci_execute($stmt);
+
         return oci_result($stmt,'PUBLIC_KEY');
     }
+
     public function getClientScope($client_id){
         if (!$clientDetails = $this->getClientDetails($client_id)) {
             return false;
         }
+
         if (isset($clientDetails['scope'])) {
             return $clientDetails['scope'];
         }
         return null;
     }
+
     public function getJti($client_id, $subject, $audience, $expires, $jti){
         $stmt = oci_parse($this->db, $sql = sprintf('SELECT * FROM %s WHERE issuer=:client_id AND subject=:subject AND audience=:audience AND expires=:expires AND jti=:jti', $this->config['jti_table']));
+
         $stmt->execute(compact('client_id', 'subject', 'audience', 'expires', 'jti'));
+
         oci_bind_by_name($stmt,':client_id',$client_id);
         oci_bind_by_name($stmt,':subject',$subject);
         oci_bind_by_name($stmt,':audience',$audience);
         oci_bind_by_name($stmt,':expires',$expires);
         oci_bind_by_name($stmt,':jti',$jti);
         oci_execute($stmt);
+
         if ($result =  array_change_key_case(oci_fetch_assoc($stmt),CASE_LOWER)) {
             return array(
                 'issuer' => $result['issuer'],
@@ -395,8 +470,10 @@ class Oracle implements
                 'jti' => $result['jti'],
             );
         }
+
         return null;
     }
+
     public function setJti($client_id, $subject, $audience, $expires, $jti){
         $stmt = oci_parse($this->db, sprintf('INSERT INTO %s (issuer, subject, audience, expires, jti) VALUES (:client_id, :subject, :audience, :expires, :jti)', $this->config['jti_table']));
         oci_bind_by_name($stmt,':client_id',$client_id);
@@ -410,6 +487,7 @@ class Oracle implements
         }
         return true;
     }
+
     /* PublicKeyInterface */
     public function getPublicKey($client_id = null){
         $stmt = oci_parse($this->db, $sql = sprintf('SELECT public_key FROM %s WHERE client_id=:client_id OR client_id IS NULL ORDER BY client_id IS NOT NULL DESC', $this->config['public_key_table']));
@@ -419,6 +497,7 @@ class Oracle implements
             return $result['public_key'];
         }
     }
+
     public function getPrivateKey($client_id = null){
         $stmt = oci_parse($this->db, $sql = sprintf('SELECT private_key FROM %s WHERE client_id=:client_id OR client_id IS NULL ORDER BY client_id IS NOT NULL DESC', $this->config['public_key_table']));
         oci_bind_by_name($stmt,':client_id',$client_id);
@@ -427,6 +506,7 @@ class Oracle implements
             return $result['private_key'];
         }
     }
+
     public function getEncryptionAlgorithm($client_id = null){
         $stmt = oci_parse($this->db, $sql = sprintf('SELECT encryption_algorithm FROM %s WHERE client_id=:client_id OR client_id IS NULL ORDER BY client_id IS NOT NULL DESC', $this->config['public_key_table']));
       	oci_bind_by_name($stmt,':client_id',$client_id);
@@ -434,8 +514,11 @@ class Oracle implements
         if ($result =  array_change_key_case(oci_fetch_assoc($stmt),CASE_LOWER)){
             return $result['encryption_algorithm'];
         }
+
         return 'RS256';
     }
+
+
     /**
      * DDL to create OAuth2 database and tables for PDO storage
      *
@@ -452,6 +535,7 @@ class Oracle implements
     	user_id               VARCHAR(80),
     	PRIMARY KEY (client_id)
     	);
+
     	CREATE TABLE {$this->config['access_token_table']} (
     	access_token         VARCHAR2(40)    NOT NULL,
     	client_id            VARCHAR2(80)    NOT NULL,
@@ -460,6 +544,7 @@ class Oracle implements
     	scope                VARCHAR2(4000),
     	PRIMARY KEY (access_token)
     	);
+
     	CREATE TABLE {$this->config['code_table']} (
     	authorization_code  VARCHAR2(40)    NOT NULL,
     	client_id           VARCHAR2(80)    NOT NULL,
@@ -470,6 +555,7 @@ class Oracle implements
     	id_token            VARCHAR2(1000),
     	PRIMARY KEY (authorization_code)
     	);
+
     	CREATE TABLE {$this->config['refresh_token_table']} (
     	refresh_token       VARCHAR2(40)    NOT NULL,
     	client_id           VARCHAR2(80)    NOT NULL,
@@ -478,6 +564,7 @@ class Oracle implements
     	scope               VARCHAR2(4000),
     	PRIMARY KEY (refresh_token)
     	);
+
     	CREATE TABLE {$this->config['user_table']} (
     	username            VARCHAR(80),
     	password            VARCHAR(80),
@@ -487,16 +574,19 @@ class Oracle implements
     	email_verified      CHAR(1),
     	scope               VARCHAR(4000)
     	);
+
     	CREATE TABLE {$this->config['scope_table']} (
     	scope               VARCHAR(80)  NOT NULL,
     	is_default          CHAR(1),
     	PRIMARY KEY (scope)
     	);
+
     	CREATE TABLE {$this->config['jwt_table']} (
     	client_id           VARCHAR(80)   NOT NULL,
     	subject             VARCHAR(80),
     	public_key          VARCHAR(2000) NOT NULL
     	);
+
     	CREATE TABLE {$this->config['jti_table']} (
     	issuer              VARCHAR(80)   NOT NULL,
     	subject             VARCHAR(80),
@@ -504,6 +594,7 @@ class Oracle implements
     	expires             VARCHAR(80)   NOT NULL,
     	jti                 VARCHAR(2000) NOT NULL
     	);
+
     	CREATE TABLE {$this->config['public_key_table']} (
     	client_id            VARCHAR(80),
     	public_key           VARCHAR(2000),
@@ -511,6 +602,7 @@ class Oracle implements
     	encryption_algorithm VARCHAR(100) DEFAULT 'RS256'
     	)
     	";
+
     	return $sql;
     }
 }
